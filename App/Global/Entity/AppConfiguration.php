9a89bf8079eee7e3884c93a5b46e1a3c61c71437
Stanislav Sergachev <89036192606@yandex.ru>
2024-09-24 12:42:47 +0300

    some fix

--- App/Global/Entity/AppConfiguration.php
+++ App/Global/Entity/AppConfiguration.php
@@ -1,7 +1,39 @@
 <?php
 
 namespace App\Global\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: modMosregVisRepository::class)]
 class AppConfiguration
 {
 
+    /***
+     * Указывает на выполнение инициализации системы
+     */
+
+    #[ORM\Column(type: 'boolean')]
+    private bool $initialComplete = false;
+    #[ORM\Column(type: 'string')]
+    private string $appLanguage = "RU-ru";
+
+    public static function getAppLanguage(): string
+    {
+        return self::$appLanguage;
+    }
+
+    public static function setAppLanguage(string $appLanguage): void
+    {
+        self::$appLanguage = $appLanguage;
+    }
+
+    public function isInitialComplete(): bool
+    {
+        return $this->initialComplete;
+    }
+
+    public function setInitialComplete(bool $initialComplete): void
+    {
+        $this->initialComplete = $initialComplete;
+    }
 }
---
+++ App/Global/Repository/AppConfigurationRepository.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Global\Repository;
+
+
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends AppConfigurationRepository<ServiceEntityRepository>
+ * @method AppConfiguration|null find($id, $lockMode = null, $lockVersion = null)
+ * @method AppConfiguration|null findOneBy(array $criteria, array $orderBy = null)
+ * @method AppConfiguration[]    findAll()
+ * @method AppConfiguration[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class AppConfigurationRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, AppConfiguration::class);
+    }
+
+    public function save(AppConfiguration $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(AppConfiguration $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+//    /**
+//     * @return AppConfiguration[] Returns an array of AppConfiguration objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('c')
+//            ->andWhere('c.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('c.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?AppConfiguration
+//    {
+//        return $this->createQueryBuilder('c')
+//            ->andWhere('c.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
