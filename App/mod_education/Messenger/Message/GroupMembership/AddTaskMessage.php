d104cb3929f6559e75d85173f501cce9923fae9c
Stanislav Sergachev <89036192606@yandex.ru>
2025-01-22 14:28:00 +0300

    Update project

---
+++ App/mod_education/Messenger/Message/GroupMembership/AddTaskMessage.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\mod_education\Messenger\Message\GroupMembership;
+
+class AddTaskMessage
+{
+    private string $content;
+
+    public function __construct(string $content)
+    {
+        $this->content = $content;
+    }
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+}
