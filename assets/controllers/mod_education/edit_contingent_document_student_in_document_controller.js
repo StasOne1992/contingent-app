80e14119b545d3eb78608e9aaf1e00c5bd257b8b
Sergachev Stainslav <sergachovstas@yandex.ru>
2025-03-03 23:20:55 +0300

    upd

--- assets/controllers/mod_education/edit_contingent_document_student_in_document_controller.js
+++ assets/controllers/mod_education/edit_contingent_document_student_in_document_controller.js
@@ -27,34 +27,55 @@ export default class extends Controller {
                 {data: 'Student.LastName'},
                 {data: 'Student.MiddleName'},
                 {data: 'StudentGroup.Name',     defaultContent: '<small>Группа не задана</small>'},
-                {data: 'StudentGroup'},
+                {data: data},
             ],
                 columnDefs:
                     [
                         {
                             targets: -1,
                             render: function (data, type, row, meta) {
-                                let groupListSelect = document.createElement("select");
+                                let currentID=crypto.randomUUID().substr(24, 8);
                                 let btnGroup= document.createElement("div");
+
+                                let groupListSelect = document.createElement("select");
+                                let btnSave = document.createElement("button");
+                                let btnCancel = document.createElement("button");
+
                                 btnGroup.classList="btn-group";
-                                groupListSelect.id = 'student:';
-                                console.log(data);
+                                btnGroup.id=currentID;
+
+
+                                groupListSelect.classList="form-select";
+                                groupListSelect.id='select-'+currentID;
+
+
+                                groupListSelect.setAttribute('studentid',data['Student']['@id']);
+                                groupListSelect.setAttribute('groupmembershipid',data['@id']);
+                                groupListSelect.add(new Option('Группа не задана',null,true))
                                 $.each(studentGroupList, function (key, value) {
                                     let option = new Option(value['Name'], value['@id']);
                                     groupListSelect.add(option);
                                 });
-                                let btnSave = document.createElement("button");
+                                if (data['StudentGroup']!==undefined )
+                                {
+                                        groupListSelect.value=data['StudentGroup']['@id'];
+                                }
+
                                 btnSave.classList='btn btn-sm btn-success';
                                 btnSave.type='button';
+                                btnSave.id='btn-save-'+currentID;
+                                btnSave.setAttribute('data-source',groupListSelect.id)
                                 btnSave.onclick =function (){
-                                    console.log('btnSave Click')
-
+                                    console.log('btnSave Click start')
+                                    let currentSelect=$(`#${this.getAttribute('data-source')}`);
+                                    console.log(currentSelect.val());
+                                    console.log('btnSave Click end')
                                 }
                                 btnSave.innerHTML='<i class="fa fa-save"></i>';
-                                let btnCancel = document.createElement("button");
                                 btnCancel.classList='btn btn-sm btn-danger';
                                 btnCancel.type='button';
                                 btnCancel.innerHTML='<i class="fa fa-cancel"></i>';
+                                btnCancel.id='btn-cancel-'+currentID;
                                 btnGroup.append(groupListSelect);
                                 btnGroup.append(btnSave);
                                 btnGroup.append(btnCancel);
