4036a6aa08ad9f3a4365f7517cb67bbb73a304c6
stasone <sergachovstas@yandex.ru>
2025-06-02 18:03:59 +0300

    Updates

--- assets/vendor/@cropper/elements/elements.index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Bundled by jsDelivr using Rollup v2.79.2 and Terser v5.39.0.
- * Original file: /npm/@cropper/elements@2.0.0/dist/elements.esm.raw.js
- *
- * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
- */
-export{default as CropperElement}from"@cropper/element";export{default as CropperCanvas}from"@cropper/element-canvas";export{default as CropperImage}from"@cropper/element-image";export{default as CropperShade}from"@cropper/element-shade";export{default as CropperHandle}from"@cropper/element-handle";export{default as CropperSelection}from"@cropper/element-selection";export{default as CropperGrid}from"@cropper/element-grid";export{default as CropperCrosshair}from"@cropper/element-crosshair";export{default as CropperViewer}from"@cropper/element-viewer";export default null;
